e0d9711f0f7b4addbeaf45d78a82c0e15e7a9fbc
RisingOrange <31575114+RisingOrange@users.noreply.github.com>
2020-10-28 00:29:36 +0100

    Threading...

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import asyncio
+import concurrent.futures
+import time
+from functools import lru_cache
 
 import cv2
 import discord
 import requests
-from functools import lru_cache
 
 from overlay import Overlay
 from overlay_monitor import active_speaker_indices
 
 TOKEN = 'Mjg1ODg0OTgwNjQxNjYwOTI5.X5gJnw.GeideTTZykXP0vuioYgo5kTLGA0'
 GUILD = 'Test'
-DELAY = 2 # delay between updating everything
+DELAY = 4 # delay between updating everything
 
 class MyClient(discord.Client):
-
-    # async def on_message(self, message):
-    #     if message.content == 'ping':
-    #         await message.channel.send('pong')
 
     def _voice_channel_members(self):
         # returns the voice channel members of the voice channel the user is in in the GUILD
@@ -33,13 +31,24 @@ class MyClient(discord.Client):
         results = []
         avatar_urls = [member.avatar_url for member in self._voice_channel_members()]
         for url in avatar_urls:
-            img = self.download_avatar_img(url)
+            img = download_avatar_img(url)
             results.append(img)
         return results
 
-    # async def on_voice_state_update(self, member, before, after):
-    #     pass
-    
+    async def on_voice_state_update(self, member, before, after):
+        global names, speaker_indices, _last_update_time
+
+        if quit:
+            raise KeyboardInterrupt('quitting')
+
+        now = time.time()
+        if (now - _last_update_time) < DELAY:
+            return
+        _last_update_time = now
+
+        names = self.voice_channel_member_names()
+        speaker_indices = active_speaker_indices(self.voice_channel_member_avatars())
+
 
 @lru_cache(20)
 def download_avatar_img(url):
@@ -51,34 +60,41 @@ def download_avatar_img(url):
     return img
 
 
-def speaker_overlays(speaker_indices):
-    results = []
-    for speaker_idx in speaker_indices:
-        results.append(XOverlay(50, 30 + 20 * speaker_idx))
-    return results
+# global variables for communication between threads
+quit = False
+names = []
+speaker_indices = []
+_last_update_time = time.time() - DELAY
 
 
-def main():
-
+def discord_client_loop():
     intents = discord.Intents.default()
     intents.members = True
-    client = MyClient(intents=intents)
+    discord_client = MyClient(intents=intents)
 
-    async def my_background_task():
-        await client.wait_until_ready()
+    discord_client_loop = asyncio.get_event_loop()
+    discord_client_loop.create_task(discord_client.start(TOKEN, bot=False))
+    return discord_client_loop
 
-        overlays = []
-        while True:
-            for overlay in overlays:
-                overlay.hide()
 
-            avatars = client.voice_channel_member_avatars()
-            overlays = speaker_overlays(active_speaker_indices(avatars))
+def main_loop():
+    while not quit:
+        print(names, speaker_indices)
+        time.sleep(2)
 
-            await asyncio.sleep(DELAY)
 
-    client.loop.create_task(my_background_task())
-    client.run(TOKEN, bot=False)
+def setup():
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
+    future1 = executor.submit(main_loop)
+    future2 = executor.submit(discord_client_loop().run_forever())
     
+    # main thread must be doing "work" to be able to catch a Ctrl+C 
+    while not future1.done() and future2.done():
+        time.sleep(1)
+
+        
 if __name__ == '__main__':
-    main()
+    try:
+        setup()
+    except KeyboardInterrupt:
+        quit = True
