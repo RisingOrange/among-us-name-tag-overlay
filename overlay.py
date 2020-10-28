4a1cc98806e26ca373be77e0a3901bffb532f1db
RisingOrange <31575114+RisingOrange@users.noreply.github.com>
2020-10-28 09:55:26 +0100

    overlay: Add root that spawns overlays periodically

--- overlay.py
+++ overlay.py
@@ -7,7 +7,7 @@ class Overlay(wx.Frame):
         style = ( wx.CLIP_CHILDREN | wx.STAY_ON_TOP | wx.FRAME_NO_TASKBAR |
                   wx.NO_BORDER | wx.FRAME_SHAPED  )
         wx.Frame.__init__(self, None, *args, **dargs, style=style)
-        self.Bind(wx.EVT_MOTION, self.OnMouse)
+        self.Bind(wx.EVT_MOTION, self.on_mouse)
         self.SetTransparent(220)
 
         # put some text with a larger bold font on it
@@ -19,7 +19,8 @@ class Overlay(wx.Frame):
 
         self.Show(True)
 
-    def OnMouse(self, event):
+
+    def on_mouse(self, event):
         """implement dragging"""
         if not event.Dragging():
             self._dragPos = None
@@ -32,7 +33,27 @@ class Overlay(wx.Frame):
             self.SetPosition( self.GetPosition() - displacement )
 
 
-if __name__ == '__main__':
+class Root(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None)
+        
+        self.overlays = []
+        self.on_timer()
+
+    def on_timer(self):
+        if len(self.overlays) > 2:
+            ov = self.overlays.pop(0)
+            ov.Destroy()
+
+        self.overlays.append(Overlay(text='FloatingOrange', size=(200, 30)))
+        wx.CallLater(2000, self.on_timer)
+
+
+def main():
     app = wx.App()
-    f = Overlay(text='FloatingOrange', size=(200, 30))
+    r = Root()
     app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
