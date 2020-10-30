b0721803fe7ae80d1ea47d92ff0f18e486adcef6
RisingOrange <31575114+RisingOrange@users.noreply.github.com>
2020-10-30 19:47:35 +0100

    Add "animation" to name tags

--- name_tag.py
+++ name_tag.py
@@ -13,6 +13,8 @@ class NameTag(wx.Frame):
         self.SetTransparent(220)
 
         self.setup_text()
+        self._is_highlight_active = False
+        self._highlight_colours = [(15, 255, 63), (0, 0, 0)]
 
         self.setup_dragging()
 
@@ -36,11 +38,30 @@ class NameTag(wx.Frame):
         self.st.Bind(wx.EVT_LEFT_DOWN, self.on_left_down)
         self.st.Bind(wx.EVT_LEFT_UP, self.on_left_up)
 
-    def highlight(self):
-        self.st.SetForegroundColour((15, 255, 63))
+    def highlight(self, recursive_call=False):
+
+        # ignore non-recursive call, when highlight is already active
+        if not recursive_call and self._is_highlight_active:
+            return
+
+        # ignore recursive call when hightlight is not active anymore
+        if recursive_call and not self._is_highlight_active:
+            return
+
+        if not recursive_call:
+            self._is_highlight_active = True
+
+        # pop colour from start and then append it back to the end,
+        # this way the colours will be rotated
+        cur_colour = self._highlight_colours.pop(0)
+        self._highlight_colours.append(cur_colour)
+
+        self.st.SetForegroundColour(cur_colour)
         self.st.Refresh()
+        wx.CallLater(500, self.highlight, recursive_call=True)
 
     def dehighlight(self):
+        self._is_highlight_active = False
         self.st.SetForegroundColour((0, 0, 0))
         self.st.Refresh()
 
