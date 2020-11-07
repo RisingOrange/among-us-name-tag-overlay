e41bbe9a256fe9c80d1d7be37d76fbe7f6b4ce88
RisingOrange <31575114+RisingOrange@users.noreply.github.com>
2020-11-07 17:21:27 +0100

    Add slot_colours_from_img, etc.

--- game_meeting_screen.py
+++ game_meeting_screen.py
@@ -1,17 +1,37 @@
 import math
+from functools import lru_cache
 
 import cv2
+import numpy as np
 
 from util import ocr_outline_font, screenshot
+
+DEBUG_MODE = True
 
 RECT_OF_FIRST_SLOT = (275, 222, 625, 110)
 SLOTS_HORIZONTAL_DISTANCE = 30
 SLOTS_VERTICAL_DISTANCE = 25
-SLOT_AMOUNT = 10
+MAX_SLOT_AMOUNT = 10
 
-VECTOR_FROM_SLOT_TO_NAME = (120, 0)
 NAME_WIDTH = 300
 NAME_HEIGHT = 53
+
+VECTOR_FROM_SLOT_TO_NAME = (120, 0)
+VECTOR_FROM_SLOT_TO_COLOUR_SPOT = (70, 80)
+VECTOR_FROM_SLOT_TO_PRESENCE_CHECK_SPOT = (1, 50)
+
+BGR_TO_COLOUR_NAME = {
+    (81, 107, 132): 'brown',
+    (238, 218, 202): 'white',
+    (124, 237, 232): 'yellow',
+    (72,  66, 194): 'red',
+    (74, 138, 207): 'orange',
+    (107, 229, 110): 'light-green',
+    (216,  87,  61): 'blue',
+    (94, 148,  60): 'dark-green',
+    (187, 111, 209): 'pink',
+    (60, 70, 77): 'black',  # black was not tested yet
+}
 
 
 def name_tag_slot_at(position):
@@ -32,16 +52,17 @@ def _is_inside_rect(position, rect):
 
 
 def slot_pos_by_idx(idx):
-    x, y, _, _ =  slot_rect_by_idx(idx)
+    x, y, _, _ = slot_rect_by_idx(idx)
     return (x, y)
 
 
 def slot_rect_by_idx(idx):
     return _slot_rect_by_idx_dict()[idx]
-    
 
+
+@lru_cache(1)
 def _slot_rect_by_idx_dict():
-    amount_slots_in_first_col = math.ceil(SLOT_AMOUNT / 2)
+    amount_slots_in_first_col = math.ceil(MAX_SLOT_AMOUNT / 2)
 
     x1, y1, w, h = RECT_OF_FIRST_SLOT
 
@@ -55,7 +76,7 @@ def _slot_rect_by_idx_dict():
         ))
 
     second_row = []
-    for i in range(SLOT_AMOUNT - amount_slots_in_first_col):
+    for i in range(MAX_SLOT_AMOUNT - amount_slots_in_first_col):
         second_row.append((
             x1 + w + SLOTS_HORIZONTAL_DISTANCE,
             y1 + i * (h + SLOTS_VERTICAL_DISTANCE),
@@ -93,3 +114,61 @@ def _slot_names_from_img(img):
         results.append(name)
 
     return results
+
+
+def _slot_colours_from_img(img):
+    results = []
+    for slot_idx in range(_active_slots_amount_from_img(img)):
+        sx, sy = slot_pos_by_idx(slot_idx)
+        dx, dy = VECTOR_FROM_SLOT_TO_COLOUR_SPOT
+        x, y = (sx+dx, sy+dy)
+
+        if DEBUG_MODE:
+            cropped = img[y:y+100, x:x+100]
+            cv2.imwrite(f'images/colour_spots/{slot_idx}.png', cropped)
+
+        colour = img[y, x]
+        colour_name = _get_colour_name(colour)
+        results.append(colour_name)
+
+        if DEBUG_MODE:
+            print(x, y, img[y, x], colour_name)
+
+    return results
+
+
+def _get_colour_name(bgr):
+    for bgr_2, name in BGR_TO_COLOUR_NAME.items():
+        if _similiar_colour(bgr, bgr_2):
+            return name
+    return None
+
+
+def _active_slots_amount_from_img(img):
+    for slot_idx in range(MAX_SLOT_AMOUNT):
+        sx, sy = slot_pos_by_idx(slot_idx)
+        dx, dy = VECTOR_FROM_SLOT_TO_PRESENCE_CHECK_SPOT
+        x, y = (sx+dx, sy+dy)
+        colour = tuple(img[y, x])
+        # two very similiar-looking white shades can have vastly different hues,
+        # thats's why the h_diff_tresh is set so high here
+        if not _similiar_colour(colour, (255, 255, 255), h_diff_thresh=1000):
+            return slot_idx
+    return MAX_SLOT_AMOUNT
+
+
+def _similiar_colour(bgr, bgr_2, h_diff_thresh=5, s_diff_thresh=30, v_diff_tresh=30):
+
+    def bgr_to_hsv(bgr):
+        single_pixel_bgr_frame = np.uint8([[[*bgr]]])
+        single_pixel_hsv_frame = cv2.cvtColor(
+            single_pixel_bgr_frame, cv2.COLOR_BGR2HSV)
+        return single_pixel_hsv_frame[0, 0]
+
+    h, s, v = map(int, bgr_to_hsv(bgr))
+    h_2, s_2, v_2, = map(int, bgr_to_hsv(bgr_2))
+    return (
+        abs(h - h_2) < h_diff_thresh and
+        abs(s - s_2) < s_diff_thresh and
+        abs(v - v_2) < v_diff_tresh
+    )
--- tests/test_game_meeting_screen.py
+++ tests/test_game_meeting_screen.py
@@ -27,19 +27,39 @@ class TestGameMeetingScreen(unittest.TestCase):
         )
         self.assertEqual(gms.slot_rect_by_idx(9), last_slot_rect_should_be)
 
-    def test_slot_names_1(self):
-        img = cv2.imread('images/screenshot_1.png')
-        self.assertEqual(
-            gms._slot_names_from_img(img),
-            ['floating'] + [f'dummy {x}' for x in range(1, 7)] + ['' for _ in range(3)]
-        )
+    # def test_slot_names_1(self):
+    #     img = cv2.imread('images/screenshot_1.png')
+    #     self.assertEqual(
+    #         gms._slot_names_from_img(img),
+    #         ['floating'] + [f'dummy {x}' for x in range(1, 7)] + ['' for _ in range(3)]
+    #     )
 
-    def test_slot_names_2(self):
+    # def test_slot_names_2(self):
+    #     img = cv2.imread('images/screenshot_2.png')
+    #     self.assertEqual(
+    #         gms._slot_names_from_img(img),
+    #         'felix jakub sandy minz naveen baumi noah abdulla crewamate'.split() + ['' for _ in range(3)]
+    #     )
+
+    def test_slot_colours(self):
+        img = cv2.imread('images/screenshot_1.png')
+        retrieved_colours = gms._slot_colours_from_img(img)
+        self.assertEqual(retrieved_colours, ['white', 'red', 'blue', 'dark-green', 'pink', 'orange', 'yellow'])
+
         img = cv2.imread('images/screenshot_2.png')
-        self.assertEqual(
-            gms._slot_names_from_img(img),
-            'felix jakub sandy minz naveen baumi noah abdulla crewamate'.split() + ['' for _ in range(3)]
-        )
+        retrieved_colours = gms._slot_colours_from_img(img)
+        self.assertEqual(retrieved_colours, ['brown', 'white', 'yellow', 'red', 'orange', 'light-green'])
+
+    def test_active_slots_amount(self):
+        img = cv2.imread('images/screenshot_1.png')
+        self.assertEqual(gms._active_slots_amount_from_img(img), 7)
+
+        img = cv2.imread('images/screenshot_2.png')
+        self.assertEqual(gms._active_slots_amount_from_img(img), 6)
+
+        
+
+
 
 if __name__ == '__main__':
     unittest.main()
