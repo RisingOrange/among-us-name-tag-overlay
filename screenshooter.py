83233f050abe54acda41bb9a1fed57a361fbf7c3
RisingOrange <31575114+RisingOrange@users.noreply.github.com>
2020-11-13 13:21:34 +0100

    Add screenshooter.py

---
+++ screenshooter.py
@@ -0,0 +1,23 @@
+import cv2
+import d3dshot
+
+class Screenshooter():
+
+    def __init__(self):
+        print('creating Screenshoter instance...')
+        # the frame_buffer_size is set to 3 to ensure thath the
+        # screenshot returned by the method screenshot_from_second_ago 
+        # is at least a second old
+        self.d3d = d3dshot.create(capture_output='numpy', frame_buffer_size=4)
+
+    def start(self):
+        self.d3d.screenshot_every(1)
+
+    def screenshot(self):
+        return cv2.cvtColor(self.d3d.screenshot(), cv2.COLOR_RGB2BGR)
+
+    def screenshot_from_second_ago(self):
+        return cv2.cvtColor(self.d3d.get_frame(3), cv2.COLOR_RGB2BGR)
+
+    def stop(self):
+        self.d3d.stop()
