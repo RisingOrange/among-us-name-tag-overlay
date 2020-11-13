8f92ec74ad726e60c9c545dc121418dca0dc34f1
RisingOrange <31575114+RisingOrange@users.noreply.github.com>
2020-11-13 13:30:04 +0100

    Create GameMeetingScreen class and add slot_colours_from_second_ago

--- game_meeting_screen.py
+++ game_meeting_screen.py
@@ -1,13 +1,14 @@
 import configparser
 import math
-import time
 from functools import lru_cache
+from cachetools import TTLCache, cached
 
 import cv2
 import numpy as np
 import pytesseract
 
-from utils import ocr_outline_font, screenshot, similiar_colour
+from screenshooter import Screenshooter
+from utils import ocr_outline_font, similiar_colour
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -51,190 +52,195 @@ TABLET_BUTTON_RECT = (1600, 480, 110, 100)
 TABLET_BUTTON_MATCH_THRESH = 0.01
 
 
-def name_tag_slot_at(position):
-    # returns the idx of the name tag slot at the position or None if there is no slot there
-    for slot_idx, slot_rect in _slot_rect_by_idx_dict().items():
-        if _is_inside_rect(position, slot_rect):
-            return slot_idx
-    return None
+class GameMeetingScreen:
+
+    def __init__(self):
+        self.scr = Screenshooter()
+        self.scr.start()
+
+    # users of the class have to close it, when their done with it
+    def close(self):
+        self.scr.stop()
+
+    # name_tag slot positions
+    def name_tag_slot_at(self, position):
+        # returns the idx of the name tag slot at the position or None if there is no slot there
+        for slot_idx, slot_rect in self._slot_rect_by_idx_dict().items():
+            if self._is_inside_rect(position, slot_rect):
+                return slot_idx
+        return None
+
+    def _is_inside_rect(self, position, rect):
+        x, y = position
+        rx, ry, rw, rh = rect
+        return (
+            rx <= x <= (rx+rw) and
+            ry <= y <= (ry+rh)
+        )
+
+    def slot_pos_by_idx(self, idx):
+        x, y, _, _ = self.slot_rect_by_idx(idx)
+        return (x, y)
+
+    def slot_rect_by_idx(self, idx):
+        return self._slot_rect_by_idx_dict()[idx]
+
+    @lru_cache(1)
+    def _slot_rect_by_idx_dict(self, ):
+        amount_slots_in_first_col = math.ceil(MAX_SLOT_AMOUNT / 2)
+
+        x1, y1, w, h = RECT_OF_FIRST_SLOT
+
+        first_row = []
+        for i in range(amount_slots_in_first_col):
+            first_row.append((
+                x1,
+                y1 + i * (h + SLOTS_VERTICAL_DISTANCE),
+                w,
+                h
+            ))
+
+        second_row = []
+        for i in range(MAX_SLOT_AMOUNT - amount_slots_in_first_col):
+            second_row.append((
+                x1 + w + SLOTS_HORIZONTAL_DISTANCE,
+                y1 + i * (h + SLOTS_VERTICAL_DISTANCE),
+                w,
+                h
+            ))
+
+        result_list = []
+        for a, b in zip(first_row, second_row):
+            result_list.extend([a, b])
+
+        return {
+            idx: rect for idx, rect in enumerate(result_list)
+        }
+
+    # ocr slot names
+    def ocr_slot_names(self, ):
+        # returns the in-game names in the order they appear on the slots
+        screenshot_img = self.scr.screenshot()
+        return self._slot_names_from_img(screenshot_img, self._active_slots_amount_from_img(screenshot_img))
+
+    def _slot_names_from_img(self, img, active_slot_amount):
+
+        def ocr_impostor_name(img):
+            mask = mask_impostor_red(img)
+
+            # invert, so that the font is black and the background white
+            inverted = cv2.inRange(mask, 0, 50)
+            return pytesseract.image_to_string(inverted).strip()
+
+        def mask_impostor_red(img):
+            # returns a mask of the image, all pixels matching the red shade of the impostor name
+            # are equal to 255, all other 0
+            img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+            result = cv2.inRange(img_hsv, np.array(
+                [120, 100, 150]), np.array([200, 300, 300]))
+            return result
+
+        def contains_impostor_red(img):
+            return 255 in mask_impostor_red(img)
+
+        results = []
+        for idx, slot_rect in sorted(list(self._slot_rect_by_idx_dict().items()))[:active_slot_amount]:
+            sx, sy, _, _ = slot_rect
+            dx, dy = VECTOR_FROM_SLOT_TO_NAME
+            nx, ny = sx+dx, sy+dy
+
+            cropped = img[ny: ny+NAME_HEIGHT, nx: nx+NAME_WIDTH]
+
+            if contains_impostor_red(cropped):
+                name = ocr_impostor_name(cropped)
+            else:
+                name = ocr_outline_font(cropped)
+
+            name = name.lower()
+            results.append(name)
+
+            if config.getboolean('GAME_MEETING_SCREEN_DEBUG_MODE'):
+                cv2.imwrite(f'cropped_names/{idx}.png', cropped)
+
+        return results
+
+    # slot colours
+    def slot_colours(self):
+        return self._slot_colours_from_img(self.scr.screenshot())
+
+    def slot_colours_from_second_ago(self):
+        return self._slot_colours_from_img(self.scr.screenshot_from_second_ago())
+
+    def _slot_colours_from_img(self, img):
+        results = []
+        for slot_idx in range(self._active_slots_amount_from_img(img)):
+            sx, sy = self.slot_pos_by_idx(slot_idx)
+            dx, dy = VECTOR_FROM_SLOT_TO_COLOUR_SPOT
+            x, y = (sx+dx, sy+dy)
+
+            if config.getboolean('GAME_MEETING_SCREEN_DEBUG_MODE'):
+                cropped = img[y:y+100, x:x+100]
+                cv2.imwrite(f'images/colour_spots/{slot_idx}.png', cropped)
+
+            colour = img[y, x]
+            colour_name = self._get_colour_name(colour)
+            results.append(colour_name)
+
+            if config.getboolean('GAME_MEETING_SCREEN_DEBUG_MODE'):
+                print(x, y, tuple(img[y, x]), colour_name)
+
+        return results
+
+    def _get_colour_name(self, bgr):
+        for bgr_2, name in BGR_TO_COLOUR_NAME.items():
+            if similiar_colour(bgr, bgr_2, h_diff_thresh=8, s_diff_thresh=120, v_diff_thresh=30):
+                return name
+        return None
+
+    def _active_slots_amount_from_img(self, img):
+        STEP = 5
+
+        for slot_idx in range(MAX_SLOT_AMOUNT):
+            slot_x, slot_y = self.slot_pos_by_idx(slot_idx)
+            stripe_x = slot_x + DX_FROM_SLOT_TO_ACTIVE_SLOT_CHECK_X
+            stripe = [
+                tuple(img[y, stripe_x])
+                for y in range(slot_y, slot_y + RECT_OF_FIRST_SLOT[3], STEP)
+            ]
+
+            # two very similiar-looking white shades can have vastly different hues,
+            # thats's why the h_diff_tresh is set so high here
+            if not any(
+                similiar_colour(colour, (255, 255, 255), h_diff_thresh=1000)
+                for colour in
+                stripe
+            ):
+                return slot_idx
+
+        return MAX_SLOT_AMOUNT
+
+    # is meeting active
+    @cached(cache=TTLCache(maxsize=1, ttl=0.8))
+    def is_meeting_active(self):
+        return self._is_meeting_active_from_img(self.scr.screenshot())
+
+    def _is_meeting_active_from_img(self, img):
+        rx, ry, rw, rh = TABLET_BUTTON_RECT
+        cropped = img[ry:ry+rh, rx:rx+rw]
+        match_result = cv2.matchTemplate(
+            cropped, TABLET_BUTTON_IMG, cv2.TM_SQDIFF_NORMED)
+        match_locations = np.where(match_result <= TABLET_BUTTON_MATCH_THRESH)
+
+        if config['GAME_MEETING_SCREEN_DEBUG_MODE']:
+            cv2.imwrite('images/cropped_tablet_button.png', cropped)
+
+        return len(match_locations[0]) > 0
 
 
-def _is_inside_rect(position, rect):
-    x, y = position
-    rx, ry, rw, rh = rect
-    return (
-        rx <= x <= (rx+rw) and
-        ry <= y <= (ry+rh)
-    )
-
-
-def slot_pos_by_idx(idx):
-    x, y, _, _ = slot_rect_by_idx(idx)
-    return (x, y)
-
-
-def slot_rect_by_idx(idx):
-    return _slot_rect_by_idx_dict()[idx]
-
-
-@lru_cache(1)
-def _slot_rect_by_idx_dict():
-    amount_slots_in_first_col = math.ceil(MAX_SLOT_AMOUNT / 2)
-
-    x1, y1, w, h = RECT_OF_FIRST_SLOT
-
-    first_row = []
-    for i in range(amount_slots_in_first_col):
-        first_row.append((
-            x1,
-            y1 + i * (h + SLOTS_VERTICAL_DISTANCE),
-            w,
-            h
-        ))
-
-    second_row = []
-    for i in range(MAX_SLOT_AMOUNT - amount_slots_in_first_col):
-        second_row.append((
-            x1 + w + SLOTS_HORIZONTAL_DISTANCE,
-            y1 + i * (h + SLOTS_VERTICAL_DISTANCE),
-            w,
-            h
-        ))
-
-    result_list = []
-    for a, b in zip(first_row, second_row):
-        result_list.extend([a, b])
-
-    return {
-        idx: rect for idx, rect in enumerate(result_list)
-    }
-
-
-def ocr_slot_names():
-    # returns the in-game names in the order they appear on the slots
-    screenshot_img = screenshot()
-    return _slot_names_from_img(screenshot_img, _active_slots_amount_from_img(screenshot_img))
-
-
-def _slot_names_from_img(img, active_slot_amount):
-
-    def ocr_impostor_name(img):
-        mask = mask_impostor_red(img)
-
-        # invert, so that the font is black and the background white
-        inverted = cv2.inRange(mask, 0, 50)
-        return pytesseract.image_to_string(inverted).strip()
-
-    def mask_impostor_red(img):
-        # returns a mask of the image, all pixels matching the red shade of the impostor name
-        # are equal to 255, all other 0
-        img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        result = cv2.inRange(img_hsv, np.array(
-            [120, 100, 150]), np.array([200, 300, 300]))
-        return result
-
-    def contains_impostor_red(img):
-        return 255 in mask_impostor_red(img)
-
-    results = []
-    for idx, slot_rect in sorted(list(_slot_rect_by_idx_dict().items()))[:active_slot_amount]:
-        sx, sy, _, _ = slot_rect
-        dx, dy = VECTOR_FROM_SLOT_TO_NAME
-        nx, ny = sx+dx, sy+dy
-
-        cropped = img[ny: ny+NAME_HEIGHT, nx: nx+NAME_WIDTH]
-
-        if contains_impostor_red(cropped):
-            name = ocr_impostor_name(cropped)
-        else:
-            name = ocr_outline_font(cropped)
-
-        name = name.lower()
-        results.append(name)
-
-        if config.getboolean('GAME_MEETING_SCREEN_DEBUG_MODE'):
-            cv2.imwrite(f'cropped_names/{idx}.png', cropped)
-
-    return results
-
-
-def slot_colours():
-    return _slot_colours_from_img(screenshot())
-
-
-def _slot_colours_from_img(img):
-    results = []
-    for slot_idx in range(_active_slots_amount_from_img(img)):
-        sx, sy = slot_pos_by_idx(slot_idx)
-        dx, dy = VECTOR_FROM_SLOT_TO_COLOUR_SPOT
-        x, y = (sx+dx, sy+dy)
-
-        if config.getboolean('GAME_MEETING_SCREEN_DEBUG_MODE'):
-            cropped = img[y:y+100, x:x+100]
-            cv2.imwrite(f'images/colour_spots/{slot_idx}.png', cropped)
-
-        colour = img[y, x]
-        colour_name = _get_colour_name(colour)
-        results.append(colour_name)
-
-        if config.getboolean('GAME_MEETING_SCREEN_DEBUG_MODE'):
-            print(x, y, tuple(img[y, x]), colour_name)
-
-    return results
-
-
-def _get_colour_name(bgr):
-    for bgr_2, name in BGR_TO_COLOUR_NAME.items():
-        if similiar_colour(bgr, bgr_2, h_diff_thresh=8, s_diff_thresh=120, v_diff_thresh=30):
-            return name
-    return None
-
-
-def _active_slots_amount_from_img(img):
-    STEP = 5
-
-    for slot_idx in range(MAX_SLOT_AMOUNT):
-        slot_x, slot_y = slot_pos_by_idx(slot_idx)
-        stripe_x = slot_x + DX_FROM_SLOT_TO_ACTIVE_SLOT_CHECK_X
-        stripe = [
-            tuple(img[y, stripe_x])
-            for y in range(slot_y, slot_y + RECT_OF_FIRST_SLOT[3], STEP)
-        ]
-
-        # two very similiar-looking white shades can have vastly different hues,
-        # thats's why the h_diff_tresh is set so high here
-        if not any(
-            similiar_colour(colour, (255, 255, 255), h_diff_thresh=1000)
-            for colour in
-            stripe
-        ):
-            return slot_idx
-
-    return MAX_SLOT_AMOUNT
-
-
-def is_meeting_active():
-    # dont do work everytime, but return the previous result, if
-    # little time has passed between the last time this had to do work
-    if time.time() - is_meeting_active.last_work_time > 1:
-        is_meeting_active.last_work_time = time.time()
-        is_meeting_active.last_result = _is_meeting_active_from_img(screenshot())
-        return is_meeting_active.last_result
-    else:
-        return is_meeting_active.last_result
-is_meeting_active.last_result = False
-is_meeting_active.last_work_time = 0
-
-
-
-def _is_meeting_active_from_img(img):
-    rx, ry, rw, rh = TABLET_BUTTON_RECT
-    cropped = img[ry:ry+rh, rx:rx+rw]
-    match_result = cv2.matchTemplate(
-        cropped, TABLET_BUTTON_IMG, cv2.TM_SQDIFF_NORMED)
-    match_locations = np.where(match_result <= TABLET_BUTTON_MATCH_THRESH)
-
-    if config['GAME_MEETING_SCREEN_DEBUG_MODE']:
-        cv2.imwrite('images/cropped_tablet_button.png', cropped)
-
-    return len(match_locations[0]) > 0
+if __name__ == '__main__':
+    scr = Screenshooter()
+    img = scr.screenshot()
+    cv2.imshow('', img)
+    cv2.waitKey(0)
+    print(GameMeetingScreen()._is_meeting_active_from_img(img))
+    scr.stop()
--- tests/test_game_meeting_screen.py
+++ tests/test_game_meeting_screen.py
@@ -1,36 +1,40 @@
 import unittest
 
 import cv2
-import game_meeting_screen as gms
+from game_meeting_screen import (RECT_OF_FIRST_SLOT, SLOTS_HORIZONTAL_DISTANCE,
+                                 SLOTS_VERTICAL_DISTANCE, GameMeetingScreen)
 
 
 class TestGameMeetingScreen(unittest.TestCase):
 
+    def setUp(self):
+        self.gms = GameMeetingScreen()
+
     def test_name_tag_slot_at(self):
-        self.assertEqual(gms.name_tag_slot_at((370, 675)), 6)
-        self.assertEqual(gms.name_tag_slot_at((1115, 270)), 1)
-        self.assertEqual(gms.name_tag_slot_at((855, 405)), 2)
+        self.assertEqual(self.gms.name_tag_slot_at((370, 675)), 6)
+        self.assertEqual(self.gms.name_tag_slot_at((1115, 270)), 1)
+        self.assertEqual(self.gms.name_tag_slot_at((855, 405)), 2)
 
     def test_slot_rect_by_idx(self):
 
         # check fist slot rect
-        self.assertEqual(gms.slot_rect_by_idx(0), gms.RECT_OF_FIRST_SLOT)
+        self.assertEqual(self.gms.slot_rect_by_idx(0), RECT_OF_FIRST_SLOT)
 
         # check last slot rect
-        x1, y1, w, h = gms.RECT_OF_FIRST_SLOT
+        x1, y1, w, h = RECT_OF_FIRST_SLOT
         last_slot_rect_should_be = (
-            x1 + w + gms.SLOTS_HORIZONTAL_DISTANCE,
-            y1 + 4 * (h + gms.SLOTS_VERTICAL_DISTANCE),
+            x1 + w + SLOTS_HORIZONTAL_DISTANCE,
+            y1 + 4 * (h + SLOTS_VERTICAL_DISTANCE),
             w,
             h
         )
-        self.assertEqual(gms.slot_rect_by_idx(9), last_slot_rect_should_be)
+        self.assertEqual(self.gms.slot_rect_by_idx(9), last_slot_rect_should_be)
 
     def test_slot_names_2(self):
         # test if normal names recognized, only one so it
         img = cv2.imread('images/screenshot_2.png')
         self.assertEqual(
-            gms._slot_names_from_img(img, 1),
+            self.gms._slot_names_from_img(img, 1),
             'felix'.split()
         )
 
@@ -38,69 +42,69 @@ class TestGameMeetingScreen(unittest.TestCase):
         # test if impostor names are recognized
         img = cv2.imread('images/screenshot_6.png')
         self.assertEqual(
-            gms._slot_names_from_img(img, 1),
+            self.gms._slot_names_from_img(img, 1),
             ['jakub']
         )
 
     def test_slot_colours_1(self):
         img = cv2.imread('images/screenshot_1.png')
-        retrieved_colours = gms._slot_colours_from_img(img)
+        retrieved_colours = self.gms._slot_colours_from_img(img)
         self.assertEqual(retrieved_colours, [
                          'white', 'red', 'blue', 'dark-green', 'pink', 'orange', 'yellow'])
 
     def test_slot_colours_2(self):
         img = cv2.imread('images/screenshot_2.png')
-        retrieved_colours = gms._slot_colours_from_img(img)
+        retrieved_colours = self.gms._slot_colours_from_img(img)
         self.assertEqual(retrieved_colours, [
                          'brown', 'white', 'yellow', 'red', 'orange', 'light-green'])
 
     def test_slot_colours_3(self):
         img = cv2.imread('images/screenshot_3.png')
-        retrieved_colours = gms._slot_colours_from_img(img)
+        retrieved_colours = self.gms._slot_colours_from_img(img)
         self.assertEqual(retrieved_colours, [
                          'orange', 'cyan', 'brown', 'blue', 'light-green', 'pink'])
 
     def test_slot_colours_4(self):
         img = cv2.imread('images/screenshot_4.png')
-        retrieved_colours = gms._slot_colours_from_img(img)
+        retrieved_colours = self.gms._slot_colours_from_img(img)
         self.assertEqual(retrieved_colours, [
                          'cyan', 'red', 'blue', 'dark-green', 'pink'])
 
     def test_slot_colours_5(self):
         img = cv2.imread('images/screenshot_5.png')
-        retrieved_colours = gms._slot_colours_from_img(img)
+        retrieved_colours = self.gms._slot_colours_from_img(img)
         self.assertEqual(retrieved_colours, [
                          'purple', 'red', 'blue', 'dark-green', 'pink'])
 
     def test_active_slots_amount_1(self):
         img = cv2.imread('images/screenshot_1.png')
-        self.assertEqual(gms._active_slots_amount_from_img(img), 7)
+        self.assertEqual(self.gms._active_slots_amount_from_img(img), 7)
 
     def test_active_slots_amount_2(self):
         img = cv2.imread('images/screenshot_2.png')
-        self.assertEqual(gms._active_slots_amount_from_img(img), 6)
+        self.assertEqual(self.gms._active_slots_amount_from_img(img), 6)
 
     def test_active_slots_amount_3(self):
         img = cv2.imread('images/screenshot_3.png')
-        self.assertEqual(gms._active_slots_amount_from_img(img), 6)
+        self.assertEqual(self.gms._active_slots_amount_from_img(img), 6)
 
     def test_is_meeting_active_from_img_1(self):
         self.assertEqual(
-            gms._is_meeting_active_from_img(
+            self.gms._is_meeting_active_from_img(
                 cv2.imread('images/screenshot_1.png')),
             True
         )
 
     def test_is_meeting_active_from_img_2(self):
         self.assertEqual(
-            gms._is_meeting_active_from_img(
+            self.gms._is_meeting_active_from_img(
                 cv2.imread('images/screenshot_2.png')),
             True
         )
 
     def test_is_meeting_active_from_img_3(self):
         self.assertEqual(
-            gms._is_meeting_active_from_img(
+            self.gms._is_meeting_active_from_img(
                 cv2.imread('images/screenshot_7.png')),
             False
         )
